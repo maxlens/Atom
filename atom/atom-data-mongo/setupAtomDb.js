8c5021bbb5049db82acbceda606adf05abf1ebe4
maxlens <maxlens@users.noreply.github.com>
2018-10-01 13:55:45 -0500

    mongo user

--- atom/atom-data-mongo/setupAtomDb.js
+++ atom/atom-data-mongo/setupAtomDb.js
@@ -1,5 +1,31 @@
 use atom
 
+db.createUser(
+   {
+     user: "atomapp",
+     pwd: "password123",
+     
+     roles: [{"role":"readWrite","db":"atom"}],
+    /* All built-in Roles 
+     Database User Roles: read|readWrite
+     Database Admin Roles: dbAdmin|dbOwner|userAdmin
+     Cluster Admin Roles: clusterAdmin|clusterManager|clusterMonitor|hostManager
+     Backup and Restoration Roles: backup|restore
+     All-Database Roles: readAnyDatabase|readWriteAnyDatabase|userAdminAnyDatabase|dbAdminAnyDatabase
+     Superuser Roles: root 
+    */
+    
+    // authenticationRestrictions: [ {
+    //     clientSource: ["192.168.0.0"],
+    //     serverAddress: ["xxx.xxx.xxx.xxx"]
+    //  } ],
+
+    //mechanisms: [ "<SCRAM-SHA-1|SCRAM-SHA-256>", ... ], 
+
+    //passwordDigestor: "<server|client>"
+   }
+)
+
 db.createCollection("user")
 
 db.user.insert([{
@@ -19,4 +45,6 @@ db.user.insert([{
     "lastLogin" : new Timestamp()
   }])
   
-  db.user.find()
+use atom
+  
+db.user.find()
