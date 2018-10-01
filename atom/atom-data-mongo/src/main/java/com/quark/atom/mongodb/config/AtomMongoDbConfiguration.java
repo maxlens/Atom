84fd95e770a2d12ea063d048e06c4683d449733d
Srinivas Hechina <shechina@Vertexinc.Com>
2018-10-01 18:02:01 -0400

    application.yml changes

--- atom/atom-data-mongo/src/main/java/com/quark/atom/mongodb/config/AtomMongoDbConfiguration.java
+++ atom/atom-data-mongo/src/main/java/com/quark/atom/mongodb/config/AtomMongoDbConfiguration.java
@@ -1,5 +1,6 @@
 package com.quark.atom.mongodb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -10,21 +11,23 @@ import com.mongodb.MongoClient;
 @EnableMongoRepositories(basePackages= {"com.quark.atom.mongodb.repository"})
 public class AtomMongoDbConfiguration extends AbstractMongoConfiguration {
 
-/*	@Value("${spring.data.mongodb.host}")
+	@Value("${spring.data.mongodb.host}")
 	private String host;
+	
 	@Value("${spring.data.mongodb.port}")
 	private int port;
+	
 	@Value("${spring.data.mongodb.database}")
-	private String database;*/
+	private String database;
 
 	@Override
 	public MongoClient mongoClient() {
-		return  new MongoClient("127.0.0.1", 27017);
+		return  new MongoClient(host, port);
 	}
 
 	@Override
 	protected String getDatabaseName() {
-		return "atom";
+		return database;
 	}
 
 }
